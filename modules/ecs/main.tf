dca3ffe44f51070ee8ca4a6635bf2124b5bf8b75
Mike Obi <141842075+obike007@users.noreply.github.com>
2024-12-23 06:41:58 +0100

    Update main.tf

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -29,3 +29,58 @@ resource "aws_ecs_service" "main" {
     security_groups = var.security_groups
   }
 }
+
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr_block
+  tags = { Name = "ecs-vpc" }
+}
+
+resource "aws_subnet" "public" {
+  count = var.public_subnets_count
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnets[count.index]
+  map_public_ip_on_launch = true
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+}
+
+resource "aws_security_group" "ecs_sg" {
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_lb" "main" {
+  name               = "ecs-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.security_group]
+  subnets            = var.subnets
+}
+
+resource "aws_lb_target_group" "main" {
+  name     = "ecs-targets"
+  port     = 5000
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
