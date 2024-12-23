468d9be523f8fe9bc675a698be599a789a0c90fa
Mike Obi <141842075+obike007@users.noreply.github.com>
2024-12-23 06:45:19 +0100

    Update variable-ecs.tf

--- modules/ecs/variable-ecs.tf
+++ modules/ecs/variable-ecs.tf
@@ -36,4 +36,22 @@ variable "desired_count" {
     default = 3
 }
 
+variable "cidr_block"{
+  type = number
+  description = "number of cidr blocks"
+  default = 3 
+}
+
+variable "public_subnets_count"{
+    type = number
+    description = "number of public subnets"
+    default = "3"
+}
+
+variable "public_subnets" {
+    type = number
+    description = "number of subnet"
+    default = "3"
+  }
+
 
